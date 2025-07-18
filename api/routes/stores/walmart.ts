615c3fd64155100a7e767ffba28cfea915d99854
daniel-xu-silk <140107537+daniel-xu-silk@users.noreply.github.com>
2025-07-18 11:13:54 -0700

    add back walmart logic

--- api/routes/stores/walmart.ts
+++ api/routes/stores/walmart.ts
@@ -24,115 +24,113 @@ function generateSignature(privateKeyPem: any, headersToSign: any) {
 }
 
 export async function getLocation(req: any, res: any) {
-  return;
-  // const { latlong } = req.query;
-  // if (!latlong) {
-  //   res.sendStatus(400);
-  //   return;
-  // }
-  // const cacheKey = `Walmart:location${latlong}`;
-  // try {
-  //   const cached = await redisClient.get(cacheKey);
-  //   if (cached) {
-  //     console.log("cache hit");
-  //     res.send(JSON.parse(cached));
-  //   } else {
-  //     const consumerId = process.env.WALMART_CONSUMER_ID;
-  //     const privateKeyPath = process.env.WALMART_PRIVATE_KEY_PATH;
-  //     if (!privateKeyPath) {
-  //       res.sendStatus(500);
-  //       return;
-  //     }
-  //     const privateKeyPem = fs.readFileSync(privateKeyPath);
-  //     const timestamp = Date.now().toString();
-  //     const headersToSign = {
-  //       "WM_CONSUMER.ID": consumerId,
-  //       "WM_CONSUMER.INTIMESTAMP": timestamp,
-  //       "WM_SEC.KEY_VERSION": 1,
-  //     };
-  //     const signature = generateSignature(privateKeyPem, headersToSign);
-  //     const headers = {
-  //       "WM_CONSUMER.ID": consumerId,
-  //       "WM_CONSUMER.INTIMESTAMP": timestamp,
-  //       "WM_SEC.KEY_VERSION": 1,
-  //       "WM_SEC.AUTH_SIGNATURE": signature,
-  //       Accept: "application/json",
-  //     };
-  //     let lat = (latlong as string).split(",")[0];
-  //     let long = (latlong as string).split(",")[1];
-  //     const walmartResponse = await axios.get(
-  //       "https://developer.api.walmart.com/api-proxy/service/affil/product/v2/stores",
-  //       {
-  //         headers,
-  //         params: {
-  //           lat: lat,
-  //           lon: long,
-  //           limit: 5,
-  //         },
-  //       }
-  //     );
-  //     await redisClient.setEx(cacheKey, 300, JSON.stringify(cached));
-  //     res.send(walmartResponse.data);
-  //   }
-  // } catch (error: any) {
-  //   console.error("Walmart API error:", error.response?.data || error.message);
-  //   res.status(500).send({ error: "Failed to fetch Walmart locations" });
-  // }
+  const { latlong } = req.query;
+  if (!latlong) {
+    res.sendStatus(400);
+    return;
+  }
+  const cacheKey = `Walmart:location${latlong}`;
+  try {
+    const cached = await redisClient.get(cacheKey);
+    if (cached) {
+      console.log("cache hit");
+      res.send(JSON.parse(cached));
+    } else {
+      const consumerId = process.env.WALMART_CONSUMER_ID;
+      const privateKeyPath = process.env.WALMART_PRIVATE_KEY_PATH;
+      if (!privateKeyPath) {
+        res.sendStatus(500);
+        return;
+      }
+      const privateKeyPem = fs.readFileSync(privateKeyPath);
+      const timestamp = Date.now().toString();
+      const headersToSign = {
+        "WM_CONSUMER.ID": consumerId,
+        "WM_CONSUMER.INTIMESTAMP": timestamp,
+        "WM_SEC.KEY_VERSION": 1,
+      };
+      const signature = generateSignature(privateKeyPem, headersToSign);
+      const headers = {
+        "WM_CONSUMER.ID": consumerId,
+        "WM_CONSUMER.INTIMESTAMP": timestamp,
+        "WM_SEC.KEY_VERSION": 1,
+        "WM_SEC.AUTH_SIGNATURE": signature,
+        Accept: "application/json",
+      };
+      let lat = (latlong as string).split(",")[0];
+      let long = (latlong as string).split(",")[1];
+      const walmartResponse = await axios.get(
+        "https://developer.api.walmart.com/api-proxy/service/affil/product/v2/stores",
+        {
+          headers,
+          params: {
+            lat: lat,
+            lon: long,
+            limit: 5,
+          },
+        }
+      );
+      await redisClient.setEx(cacheKey, 300, JSON.stringify(cached));
+      res.send(walmartResponse.data);
+    }
+  } catch (error: any) {
+    console.error("Walmart API error:", error.response?.data || error.message);
+    res.status(500).send({ error: "Failed to fetch Walmart locations" });
+  }
 }
 
 export async function getProduct(req: any, res: any) {
-  return;
-  // let { item } = req.query;
-  // if (!item) {
-  //   res.sendStatus(400);
-  //   return;
-  // }
-  // let cacheKey = `Walmart:items${item}`;
-  // try {
-  //   const cached = await redisClient.get(cacheKey);
-  //   if (cached) {
-  //     console.log("cache hit");
-  //     res.send(JSON.parse(cached));
-  //   } else {
-  //   }
-  //   const consumerId = process.env.WALMART_CONSUMER_ID;
-  //   const privateKeyPath = process.env.WALMART_PRIVATE_KEY_PATH;
-  //   if (!privateKeyPath) {
-  //     res.sendStatus(500);
-  //     return;
-  //   }
-  //   const privateKeyPem = fs.readFileSync(privateKeyPath);
-  //   const timestamp = Date.now().toString();
-  //   const headersToSign = {
-  //     "WM_CONSUMER.ID": consumerId,
-  //     "WM_CONSUMER.INTIMESTAMP": timestamp,
-  //     "WM_SEC.KEY_VERSION": 1,
-  //   };
-  //   const signature = generateSignature(privateKeyPem, headersToSign);
-  //   const headers = {
-  //     "WM_CONSUMER.ID": consumerId,
-  //     "WM_CONSUMER.INTIMESTAMP": timestamp,
-  //     "WM_SEC.KEY_VERSION": 1,
-  //     "WM_SEC.AUTH_SIGNATURE": signature,
-  //     Accept: "application/json",
-  //   };
-  //   const walmartResponse = await axios.get(
-  //     "https://developer.api.walmart.com/api-proxy/service/affil/product/v2/search",
-  //     {
-  //       headers,
-  //       params: {
-  //         query: item,
-  //       },
-  //     }
-  //   );
-  //   await redisClient.setEx(
-  //     cacheKey,
-  //     300,
-  //     JSON.stringify(walmartResponse.data)
-  //   );
-  //   res.send(walmartResponse.data);
-  // } catch (error: any) {
-  //   console.error("Walmart API error:", error.response?.data || error.message);
-  //   res.status(500).send({ error: "Failed to fetch Walmart products" });
-  // }
+  let { item } = req.query;
+  if (!item) {
+    res.sendStatus(400);
+    return;
+  }
+  let cacheKey = `Walmart:items${item}`;
+  try {
+    const cached = await redisClient.get(cacheKey);
+    if (cached) {
+      console.log("cache hit");
+      res.send(JSON.parse(cached));
+    } else {
+    }
+    const consumerId = process.env.WALMART_CONSUMER_ID;
+    const privateKeyPath = process.env.WALMART_PRIVATE_KEY_PATH;
+    if (!privateKeyPath) {
+      res.sendStatus(500);
+      return;
+    }
+    const privateKeyPem = fs.readFileSync(privateKeyPath);
+    const timestamp = Date.now().toString();
+    const headersToSign = {
+      "WM_CONSUMER.ID": consumerId,
+      "WM_CONSUMER.INTIMESTAMP": timestamp,
+      "WM_SEC.KEY_VERSION": 1,
+    };
+    const signature = generateSignature(privateKeyPem, headersToSign);
+    const headers = {
+      "WM_CONSUMER.ID": consumerId,
+      "WM_CONSUMER.INTIMESTAMP": timestamp,
+      "WM_SEC.KEY_VERSION": 1,
+      "WM_SEC.AUTH_SIGNATURE": signature,
+      Accept: "application/json",
+    };
+    const walmartResponse = await axios.get(
+      "https://developer.api.walmart.com/api-proxy/service/affil/product/v2/search",
+      {
+        headers,
+        params: {
+          query: item,
+        },
+      }
+    );
+    await redisClient.setEx(
+      cacheKey,
+      300,
+      JSON.stringify(walmartResponse.data)
+    );
+    res.send(walmartResponse.data);
+  } catch (error: any) {
+    console.error("Walmart API error:", error.response?.data || error.message);
+    res.status(500).send({ error: "Failed to fetch Walmart products" });
+  }
 }
